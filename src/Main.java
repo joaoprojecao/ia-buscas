762ec69816d5fc75983a6a329d25d9fda00feb35
joaoprojecao <102630809+joaoprojecao@users.noreply.github.com>
2022-05-24 22:06:14 -0300

    Busca Hillclimbing

--- src/Main.java
+++ src/Main.java
@@ -14,119 +14,135 @@ import java.util.Scanner;
 
 public class Main {
 
-	@SuppressWarnings("rawtypes")
-	public static void main(String[] args) throws IOException {
+    @SuppressWarnings("rawtypes")
+    public static void main(String[] args) throws IOException {
 
 //		char[] cfgFim = {' ','2','3','1','4','6','7','5','8'};
-		char[] cfgIni = {'2','4','3','7','1','6','5',' ','8'};
-		char[] cfgFim = {'2','4','3','7','1','6','5',' ','8'};
+        char[] cfgIni = {'2', ' ', '3', '1', '4', '6', '7', '5', '8'};
+        char[] cfgFim = {'1', '2', '3', '4', '5', '6', '7', '8', ' '};
 //		char[] cfgIni = {'2','3',' ','7','4','1','5','8','6'};
-		//char[] cfgIni = {'7','2','3','4',' ','1','5','8','6'}; // OutOfMemory
+        //char[] cfgIni = {'7','2','3','4',' ','1','5','8','6'}; // OutOfMemory
 
-		Puzzle8 puzzleInicial = new Puzzle8();
-		puzzleInicial.setEstado(cfgIni);
-		puzzleInicial.setCusto(0);
-		puzzleInicial.setAvaliacao( puzzleInicial.heuristica(Puzzle8.TABULEIRO_ORGANIZADO) );
+        boolean canClose = false;
+		while (!canClose) {
+			Puzzle8 puzzleInicial = new Puzzle8();
+			puzzleInicial.setEstado(cfgIni);
+			puzzleInicial.setCusto(0);
+			puzzleInicial.setAvaliacao(puzzleInicial.heuristica(Puzzle8.TABULEIRO_ORGANIZADO));
 
-		Puzzle8 puzzleFinal = new Puzzle8();
-		puzzleFinal.setEstado(cfgFim);
-		puzzleFinal.setCusto(0);
-		puzzleFinal.setAvaliacao(0);
+			Puzzle8 puzzleFinal = new Puzzle8();
+			puzzleFinal.setEstado(cfgFim);
+			puzzleFinal.setCusto(0);
+			puzzleFinal.setAvaliacao(0);
 
-		Scanner option = new Scanner(System.in);
+            //MENU
+            System.out.println("1 - Busca em Profundidade Limitada");
+            System.out.println("2 - Busca em Profundidade");
+            System.out.println("3 - Busca em Profundidade Limitada Interativa");
+            System.out.println("4 - Busca em Largura");
+            System.out.println("5 - Busca em Best First");
+            System.out.println("6 - Busca A*");
+            System.out.println("7 - Hillclimbing");
+			System.out.println("8 - Sair");
+            Scanner option = new Scanner(System.in);
+            int opcao = option.nextInt();
 
-		//MENU
-		System.out.println("1 - Busca em Profundidade Limitada");
-		System.out.println("2 - Busca em Profundidade");
-		System.out.println("3 - Busca em Profundidade Limitada Interativa");
-		System.out.println("4 - Busca em Largura");
-		System.out.println("5 - Busca em Best First");
-		System.out.println("6 - Busca A*");
-		System.out.println("7 - Sair");
-		int opcao = option.nextInt();
+            switch (opcao) {
+                case 1:
+                    BuscaEmProfundidadeLimitada buscaFundaLimitada = new BuscaEmProfundidadeLimitada();
+                    buscaFundaLimitada.setLimite(10);
+                    buscaFundaLimitada.setInicio(puzzleInicial);
+                    buscaFundaLimitada.setObjetivo(puzzleFinal);
+                    buscaFundaLimitada.buscar();
+                    for (Estado e : buscaFundaLimitada.getCaminhoSolucao()) {
+                        System.out.println(e);
+                    }
+                    break;
 
-		if (opcao == 7) System.exit(0);
+                case 2:
+                    BuscaCega buscaFunda = new BuscaEmProfundidade();
+                    buscaFunda.setInicio(puzzleInicial);
+                    buscaFunda.setObjetivo(puzzleFinal);
+                    buscaFunda.buscar();
+                    for (Estado e : buscaFunda.getCaminhoSolucao()) {
+                        System.out.println(e);
+                    }
+                    break;
 
-		switch (opcao) {
-			case 1:
-				BuscaEmProfundidadeLimitada buscaFundaLimitada = new BuscaEmProfundidadeLimitada();
-				buscaFundaLimitada.setLimite(10);
-				buscaFundaLimitada.setInicio(puzzleInicial);
-				buscaFundaLimitada.setObjetivo(puzzleFinal);
-				buscaFundaLimitada.buscar();
-				for (Estado e : buscaFundaLimitada.getCaminhoSolucao()) {
-					System.out.println(e);
-				}
-				break;
+                case 3:
+                    System.out.println("Digite o Minimo: \n");
+                    BufferedReader input1 = new BufferedReader(
+                            new InputStreamReader(System.in));
+                    int input1Valor = Integer.valueOf(input1.readLine());
 
-			case 2:
-				BuscaCega buscaFunda = new BuscaEmProfundidade();
-				buscaFunda.setInicio(puzzleInicial);
-				buscaFunda.setObjetivo(puzzleFinal);
-				buscaFunda.buscar();
-				for (Estado e : buscaFunda.getCaminhoSolucao()) {
-					System.out.println(e);
-				}
-				break;
+                    System.out.println("Digite o Limite: \n");
+                    BufferedReader input2 = new BufferedReader(
+                            new InputStreamReader(System.in));
+                    int input2Valor = Integer.valueOf(input2.readLine());
+                    for (int i = input1Valor; i <= input2Valor; i++) {
+                        BuscaEmProfundidadeLimitada buscaEmProfundidadeLimitada = new BuscaEmProfundidadeLimitada();
+                        buscaEmProfundidadeLimitada.setLimite(i);
+                        buscaEmProfundidadeLimitada.setInicio(puzzleInicial);
+                        buscaEmProfundidadeLimitada.setObjetivo(puzzleFinal);
+                        buscaEmProfundidadeLimitada.buscar();
+                        for (Estado e : buscaEmProfundidadeLimitada.getCaminhoSolucao()) {
+                            System.out.println(e);
+                        }
+                    }
+                    break;
 
-			case 3:
-				System.out.println("Digite o Minimo: \n");
-				BufferedReader input1 = new BufferedReader(
-						new InputStreamReader(System.in));
-				int input1Valor = Integer.valueOf(input1.readLine());
+                case 4:
+                    BuscaCega busca = new BuscaEmLargura();
+                    busca.setInicio(puzzleInicial);
+                    busca.setObjetivo(puzzleFinal);
+                    busca.buscar();
+                    for (Estado e : busca.getCaminhoSolucao()) {
+                        System.out.println(e);
+                    }
+                    break;
 
-				System.out.println("Digite o Limite: \n");
-				BufferedReader input2 = new BufferedReader(
-						new InputStreamReader(System.in));
-				int input2Valor = Integer.valueOf(input2.readLine());
-				for (int i = input1Valor; i <= input2Valor; i++) {
-					BuscaEmProfundidadeLimitada buscaEmProfundidadeLimitada = new BuscaEmProfundidadeLimitada();
-					buscaEmProfundidadeLimitada.setLimite(i);
-					buscaEmProfundidadeLimitada.setInicio(puzzleInicial);
-					buscaEmProfundidadeLimitada.setObjetivo(puzzleFinal);
-					buscaEmProfundidadeLimitada.buscar();
-					for (Estado e : buscaEmProfundidadeLimitada.getCaminhoSolucao()) {
-						System.out.println(e);
-					}
-				}
-				break;
+                case 5:
+                    BestFirst bestFirst = new BestFirst();
+                    bestFirst.setInicio(puzzleInicial);
+                    bestFirst.setObjetivo(puzzleFinal);
 
-			case 4:
-				BuscaCega busca = new BuscaEmLargura();
-				busca.setInicio(puzzleInicial);
-				busca.setObjetivo(puzzleFinal);
-				busca.buscar();
-				for (Estado e : busca.getCaminhoSolucao()) {
-					System.out.println(e);
-				}
-				break;
+                    bestFirst.buscar();
+                    for (Estado e : bestFirst.getCaminhoSolucao()) {
+                        System.out.println(e);
+                    }
 
-			case 5:
-				BestFirst bestFirst = new BestFirst();
-				bestFirst.setInicio(puzzleInicial);
-				bestFirst.setObjetivo(puzzleFinal);
+                    break;
 
-				bestFirst.buscar();
-				for (Estado e : bestFirst.getCaminhoSolucao()) {
-					System.out.println(e);
-				}
+                case 6:
+                    AStar buscaAEstrela = new AStar();
+                    buscaAEstrela.setInicio(puzzleInicial);
+                    buscaAEstrela.setObjetivo(puzzleFinal);
+                    buscaAEstrela.buscar();
+                    for (Estado e : buscaAEstrela.getCaminhoSolucao()) {
+                        System.out.println(e);
+                    }
 
-				break;
+                    break;
 
-			case 6:
-				AStar buscaAEstrela = new AStar();
-				buscaAEstrela.setInicio(puzzleInicial);
-				buscaAEstrela.setObjetivo(puzzleFinal);
-				buscaAEstrela.buscar();
-				for (Estado e : buscaAEstrela.getCaminhoSolucao()) {
-					System.out.println(e);
-				}
+                case 7:
+                    HillClimbing buscaHill = new HillClimbing();
+                    buscaHill.setInicio(puzzleInicial);
+                    buscaHill.setObjetivo(puzzleFinal);
+                    buscaHill.buscar();
+                    for (Estado e : buscaHill.getCaminhoSolucao()) {
+                        System.out.println(e);
+                    }
 
-				break;
-			default:
-				System.out.print("\nOpção Inválida\n\n");
-				break;
-		}
-		System.exit(0);
-	}
+                    break;
+
+                case 8:
+                    canClose = true;
+
+                default:
+                    System.out.print("\nOpção Inválida\n\n");
+                    break;
+            }
+        }
+        System.exit(0);
+    }
 }
